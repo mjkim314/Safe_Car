9118a370608b4a2a9486a287a629973d459ed1a0
goodbear02 <48387271+goodbear02@users.noreply.github.com>
2024-12-03 21:23:50 +0900

    Update controller.c - 클라이언트 출력하는 lcd기능 추가

--- joystick/controller.c
+++ joystick/controller.c
@@ -26,7 +26,7 @@ void* controller_to_car_output(void* arg) {
 
     while (1) {
 
-        if (count % 10 == 0) {
+        if (count % 1 == 0) {
 
             int* temp = readController(joystick_fd);
             memcpy(joy_data, temp, sizeof(joy_data)); // 배열 데이터 복사
@@ -35,7 +35,7 @@ void* controller_to_car_output(void* arg) {
 
             { //1초 후 부터 조이스틱 값 전송 가능
                 //저역통과 필터 단게
-                printf("before   -    X: %d  Y: %d   B: %d ||     ", joy_data[0], joy_data[1], joy_data[2]);
+                //printf("before   -    X: %d  Y: %d   B: %d ||     ", joy_data[0], joy_data[1], joy_data[2]);
 
                 joy_data[0] = (int)(alpha * joy_data[0] + (1 - alpha) * prev_joy_data[0]);
                 joy_data[1] = (int)(alpha * joy_data[1] + (1 - alpha) * prev_joy_data[1]);
@@ -82,8 +82,14 @@ void* controller_to_car_output(void* arg) {
                 else
                     joy_data[2] = 1;
 
-                printf("after   -    X: %d  Y: %d  B : %d\n", joy_data[0], joy_data[1], joy_data[2]);
+                //printf("after   -    X: %d  Y: %d  B : %d\n", joy_data[0], joy_data[1], joy_data[2]);
 
+
+                if (count < 300)
+                {
+                    joy_data[0] = 0;
+                    joy_data[1] = 0;
+                }
                 for (int i = 0; i < 3; i++) {
                     joy_data[i] = htonl((int)joy_data[i]);
                 }
@@ -103,53 +109,48 @@ void* controller_to_car_output(void* arg) {
 
 void* check_clnt_lcd(void* arg) {
     int serv_sock = *(int*)arg;
-    char buffer[256];
+    char buffer[36];
+    char prev_buffer[36];
+    int count = 0;
+    struct timespec delay;
+    delay.tv_sec = 0;
+    delay.tv_nsec = 10000000;
 
     while (1) {
-        struct timespec delay;
-        delay.tv_sec = 0;
-        delay.tv_nsec = 10000000;
-        int count = 0;
 
-        if (count % 10 == 0) {
-            ssize_t bytes_read = read(serv_sock, buffer, sizeof(buffer) - 1);
+
+        if (count % 50 == 0) {
+            ssize_t bytes_read = read(serv_sock, buffer, sizeof(buffer));
 
             if (bytes_read > 0) {
-                char str1[17] = { 0 }; // 처음 16자를 저장할 공간 (NULL 문자 포함)
-                char str2[37] = { 0 }; // 나머지 문자를 저장할 공간
-                if (strlen(buffer) > 16) {
-                    strncpy(str1, buffer, 16); // 처음 16자 복사
-                    str1[16] = '\0'; // NULL 문자 추가
-                    strncpy(str2, buffer + 16, sizeof(str2) - 1); // 17번째 문자부터 복사
-                    str2[sizeof(str2) - 1] = '\0'; // NULL 문자 추가
+                printf("tot : %s\n", buffer);
+                if (strcmp(prev_buffer, buffer) != 0) {
+                    char str1[18] = { 0 }; // 처음 16자를 저장할 공간 (NULL 문자 포함)
+                    char str2[18] = { 0 }; // 나머지 문자를 저장할 공간
+                    if (strlen(buffer) > 16) {
+                        strncpy(str1, buffer, 16); // 처음 16자 복사
+                        str1[16] = '\0'; // NULL 문자 추가
+                        strncpy(str2, buffer + 16, sizeof(str2) - 1); // 17번째 문자부터 복사
+                        str2[sizeof(str2) - 1] = '\0'; // NULL 문자 추가
+                    }
+                    else {
+                        strncpy(str1, buffer, sizeof(str1) - 1); // buffer 전체를 str1에 복사
+                        str1[sizeof(str1) - 1] = '\0'; // NULL 문자 추가
+                    }
+
+                    lcd_init();
+                    lcd_m(LINE1);
+                    print_str(str1);
+
+                    lcd_m(LINE2);
+                    print_str(str2);
+                    usleep(100000);
                 }
-                else {
-                    strncpy(str1, buffer, sizeof(str1) - 1); // buffer 전체를 str1에 복사
-                    str1[sizeof(str1) - 1] = '\0'; // NULL 문자 추가
-                }
-
-                lcd_init();
-                lcd_m(LINE1);
-                print_str(str1);
-
-                lcd_m(LINE2);
-                print_str(str2);
-
-                
-
-            }
-            else
-            {
-                
-                lcd_init();
-                lcd_m(LINE1);
-                print_str("Lost connection to server");
-                usleep(1000000);
-                pthread_exit(NULL);
+                 
+                strcpy(prev_buffer, buffer);
 
             }
         }
-        memset(buffer, 0, sizeof(buffer));
         count++;
         nanosleep(&delay, NULL);
 
@@ -162,6 +163,7 @@ int main(int argc, char* argv[]) {
 
     struct sockaddr_in car_serv_addr;
 
+
     if (argc != 2) {
         printf("Usage : %s <IP>\n", argv[0]);
         exit(EXIT_FAILURE);
@@ -170,6 +172,12 @@ int main(int argc, char* argv[]) {
     if ((car_serv_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
+    }
+
+    fd = wiringPiI2CSetup(LCD_ADDR);  // I2C 장치 설정 (주소는 0x27)
+    if (fd == -1) {
+        printf("LCD initialization failed.\n");
+        return -1;
     }
 
     memset(&car_serv_addr, 0, sizeof(car_serv_addr));
